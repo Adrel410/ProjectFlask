79735775f29a1d91a015a6b1f0542a46ca760a5e
Adrel410 <ogundipe_adeola@columbusstate.edu>
2023-09-30 21:20:13 +0000

    added job

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template   
 from flask import Flask, render_template, request
 
-app = Flask(__name__)             
+app = Flask(__name__)
+
+# Dummy data for job postings (use the API's database)
+job_postings = [
+    {"id": 1, "title": "Web Developer", "description": "Full-stack web developer position"},
+    {"id": 2, "title": "Data Scientist", "description": "Data science role"},
+    {"id": 3, "title": "UX Designer", "description": "User experience designer needed"},
+]
 
 @app.route('/')
 def home():
@@ -21,6 +28,18 @@ def job_details(job_id):
     # Fetch job details based on job_id from database
     job_details_data = {'Doctor': 'Job 1', 'description': 'Description 1'}
     return render_template('job_details.html', job=job_details_data)   
+
+@app.route('/post_job', methods=['GET', 'POST'])
+def post_job():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        # In the real application, you would save the job posting to a database.
+        # we added it to the dummy data.
+        job_id = len(job_postings) + 1
+        job_postings.append({"id": job_id, "title": title, "description": description})
+        return redirect(url_for('index'))
+    return render_template('post_job.html')
 
 @app.route("/about")
 def about():
